e1b88bc7097e0bcf7960001a9d458d14dc3d429b
cens_a <ejungwoo@ibs.re.kr>
2025-07-03 03:57:06 +0000

    change first example

--- simulation/analysis_header.h
+++ simulation/analysis_header.h
@@ -1,16 +1,11 @@
 #ifndef analysis_header_h
 #define analysis_header_h
 
-//TString default_file_name1 = "data/cssu_4He_5.486_5.486_10deg_CF4_30torr_d10_10000.dat";
-//TString default_file_name1 = "data/cssu_4He_8_3_20deg_CF4_30torr_d5_10000.dat";
-//TString default_file_name1 = "data/cssu_proton_8_3_20deg_CF4_30torr_d5_10000.dat";
-//TString default_file_name1 = "data/cssu_3H_8_3_20deg_CF4_30torr_d5_10000.dat";
-TString default_file_name1 = "data/cssu_3He_8_3_20deg_CF4_30torr_d5_10000.dat";
+TString default_file_name1 = "data/cssu_4He_5.5_5.5_5deg_CF4_30torr_d10_10000.dat";
+//TString default_file_name1 = "data/cssu_3He_8_3_20deg_CF4_30torr_d5_10000.dat";
 
-//TString default_file_name2 = "";
-TString default_file_name2 = "data/cssu_4He_8_3_20deg_CF4_30torr_d5_10000.dat";
-//TString default_file_name2 = "data/cssu_proton_8_3_20deg_CF4_30torr_d5_10000.dat";
-//TString default_file_name2 = "data/cssu_proton_8_3_20deg_CF4_30torr_d5_10000.dat";
+TString default_file_name2 = "";
+//TString default_file_name2 = "data/cssu_4He_8_3_20deg_CF4_30torr_d5_10000.dat";
 
 double energy_resolution = 0.01; // =sigma/mean
 
